55df44c1d5c7bc22cf653010c930541f20f94ffa
ajit51 <ajitk433@gmail.com>
2022-08-21 17:18:49 +0530

    first commit

---
+++ src/main/java/com/example/filestorages3service/FileStorageS3ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.filestorages3service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageS3ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageS3ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filestorages3service/FileStorageS3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filestorages3service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageS3ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
